e6962fd5318ea6e2d9c0d98750657430b9233025
Dev-Joco <csr8367@hanmail.net>
2022-11-27 21:52:13 +0900

    feat(view): OutputView.printFinalWinner() 기능 구현

--- src/main/kotlin/racingcar/view/strings/Messages.kt
+++ src/main/kotlin/racingcar/view/strings/Messages.kt
@@ -4,7 +4,8 @@ enum class Messages(val message: String) {
 
     InputCarNames("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     InputTryCount("시도할 회수는 몇회인가요?"),
-    GameResult("실행 결과"),;
+    GameResult("실행 결과"),
+    FinalWinner("최종 우승자 : %s");
 
     override fun toString(): String = message
 }
