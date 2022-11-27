0c2e5a704563a167a235a1879c303ef221782f22
Dev-Joco <csr8367@hanmail.net>
2022-11-27 23:21:53 +0900

    fix(util): isUpperOrLower 함수 조건 추가

--- src/main/kotlin/racingcar/util/StringExt.kt
+++ src/main/kotlin/racingcar/util/StringExt.kt
@@ -8,4 +8,5 @@ inline fun String.toIntOrThrow(lazyMessage: () -> Any): Int {
     return requireNotNull(toIntOrNull(), lazyMessage)
 }
 
-fun String.isUpperOrLower(): Boolean = all { it.isUpperCase() || it.isLowerCase() }
+fun String.isUpperOrLower(): Boolean =
+    if (isEmpty()) false else all { it.isUpperCase() || it.isLowerCase() }
