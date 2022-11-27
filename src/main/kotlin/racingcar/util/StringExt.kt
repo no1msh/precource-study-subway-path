a4956a2d90496d76710a430023dd57f038781c91
Dev-Joco <csr8367@hanmail.net>
2022-11-27 17:04:14 +0900

    feat(view): InputView.readTryCount() 기능 구현

--- src/main/kotlin/racingcar/util/StringExt.kt
+++ src/main/kotlin/racingcar/util/StringExt.kt
@@ -1,3 +1,11 @@
 package racingcar.util
 
+fun String.toIntOrThrow(): Int {
+    return requireNotNull(toIntOrNull())
+}
+
+inline fun String.toIntOrThrow(lazyMessage: () -> Any): Int {
+    return requireNotNull(toIntOrNull(), lazyMessage)
+}
+
 fun String.isUpperOrLower(): Boolean = all { it.isUpperCase() || it.isLowerCase() }
