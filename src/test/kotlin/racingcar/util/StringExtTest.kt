1bb754387ae6cbd835a0a0c1edc764b43a707190
Dev-Joco <csr8367@hanmail.net>
2022-11-27 23:10:35 +0900

    test(util): toIntOrThrow 함수 기능 테스트

---
+++ src/test/kotlin/racingcar/util/StringExtTest.kt
@@ -0,0 +1,30 @@
+package racingcar.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class StringExtTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "123", "0", "-24",
+        Int.MAX_VALUE.toString(), Int.MIN_VALUE.toString(),
+    ])
+    fun `toIntOrThrow_숫자_정상`(text: String) {
+        assertThat(text.toIntOrThrow()).isEqualTo(text.toInt())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "a", "ㄱ",
+        "10a", "10.0", "10,0", "11-0", "1 0", "10 ",
+        Long.MIN_VALUE.toString(), Long.MAX_VALUE.toString()
+    ])
+    fun `toIntOrThrow_에러`(text: String) {
+        assertThrows<IllegalArgumentException> {
+            text.toIntOrThrow()
+        }
+    }
+}
