c7b4abb3bc6abadae65099d69c61143b76e3de71
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-28 18:23:30 +0900

    refactor(Car): get 함수 삭제 및 비교 함수 동반 객체로 이관

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -7,10 +7,6 @@ class Car(private val name: String) {
     private var position: Int = 0
 
     // 추가 기능 구현부
-    fun getName(): String = name
-
-    fun getPosition(): Int = position
-
     fun drive() {
         val randomNumber = pickNumber()
         if (randomNumber >= CONDITION) {
@@ -34,5 +30,26 @@ class Car(private val name: String) {
         private const val START_NUMBER = 0
         private const val END_NUMBER = 9
         private const val CONDITION = 4
+
+        fun compare(cars: List<Car>): MutableList<String> {
+            val sortedCars = sortByPosition(cars)
+            val result: MutableList<String> = mutableListOf()
+            val winnerCondition = sortedCars[0].second
+            sortedCars.forEach {
+                if (it.second == winnerCondition) {
+                    result.add(it.first)
+                }
+            }
+            return result
+        }
+
+        private fun sortByPosition(cars: List<Car>): MutableList<Pair<String, Int>> {
+            val sortedCars: MutableList<Pair<String, Int>> = mutableListOf()
+            for (car in cars) {
+                sortedCars.add(car.name to car.position)
+            }
+            sortedCars.sortByDescending { it.second }
+            return sortedCars
+        }
     }
 }
