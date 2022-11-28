21af8ae9511af1d10a976f37d706e7e40677aa91
Yerin <myheaven_b@naver.com>
2022-11-28 15:36:06 +0900

    fix: exception 발생 부분 수정

--- src/main/kotlin/racingcar/Exceptions.kt
+++ src/main/kotlin/racingcar/Exceptions.kt
@@ -1,15 +1,29 @@
 package racingcar
 
 class Exceptions {
-    val outputView = OutputView()
+    private val outputView = OutputView()
 
     fun validateInputNames(input: List<String>): Boolean {
-        require(input.size == input.distinct().size) {
-            outputView.printInputDuplicateError()
+        if (validateInputSize(input) || validateInputDuplicate(input))
+            return true
+        return false
+    }
+
+    private fun validateInputDuplicate(input: List<String>): Boolean {
+        try {
+            require(input.all { it.length <= 5 })
+        } catch (e: IllegalArgumentException) {
+            outputView.printInputLengthError()
             return true
         }
-        require(input.all { it.length <= 5 }) {
-            outputView.printInputLengthError()
+        return false
+    }
+
+    private fun validateInputSize(input: List<String>): Boolean{
+        try {
+            require(input.size == input.distinct().size)
+        } catch (e: IllegalArgumentException) {
+            outputView.printInputDuplicateError()
             return true
         }
         return false
