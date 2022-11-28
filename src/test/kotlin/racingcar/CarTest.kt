8da0d575b3bba5ea10d49a7fe9c04bbeacdb6106
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-28 18:25:21 +0900

    refactor(CarTest): 사용하지 않는 변수 삭제

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,7 +1,5 @@
 package racingcar
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class CarTest {
@@ -18,6 +16,6 @@ internal class CarTest {
     @Test
     fun `랜덤 숫자 생성 테스트`() {
         val car = Car("test")
-        val number = car.pickNumber().also { println(it) }
+        car.pickNumber().also { println(it) }
     }
 }
