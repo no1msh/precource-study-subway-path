b4daf82264491bee236839e4beb3a1f0920adee7
amb <mikaelh1651@gmail.com>
2022-11-28 15:44:08 +0900

    feat(InputView): 자동차 명 입력 받는 기능 추가

---
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -0,0 +1,26 @@
+package racingcar.ui
+
+import camp.nextstep.edu.missionutils.Console
+import racingcar.enums.ErrorMessages
+
+class InputView {
+    tailrec fun readCarName(): List<String> {
+        val carNames = Console.readLine().trim().split(",")
+        return try {
+            validateCarNames(carNames)
+            carNames
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readCarName()
+        }
+    }
+
+    private fun validateCarNames(carNames: List<String>) {
+        require(carNames.all { it.length <= 5 }) {
+            ErrorMessages.LENGTH_EXCEPTION.toString()
+        }
+        require(carNames.distinct().count() == carNames.size) {
+            ErrorMessages.DISTINCT_EXCEPTION.toString()
+        }
+    }
+}
