f6ae83b6f02aa61b4aac85f72c74156734b753f3
meltnia <themtale@gmail.com>
2022-11-29 21:47:07 +0900

    feat : 차 입력에 관한 함수 일부 구현

--- src/main/kotlin/racingcar/IO/RacingInput.kt
+++ src/main/kotlin/racingcar/IO/RacingInput.kt
@@ -1,8 +1,22 @@
 package racingcar.IO
 
+import camp.nextstep.edu.missionutils.Console
+
 object RacingInput {
-
-
+    fun inputCar(): String {
+        var check = false
+        var cars = ""
+        while (!check) {
+            try {
+                val cars = Console.readLine()
+                //에러체크
+                check = true
+            } catch (error: IllegalArgumentException) {
+                println(error.message)
+            }
+        }
+        return cars
+    }
 
 
 }
