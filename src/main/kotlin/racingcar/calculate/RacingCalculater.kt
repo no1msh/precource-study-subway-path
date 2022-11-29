fde6d4686097065a59e1f14f3b8b445658c5a9f2
meltnia <themtale@gmail.com>
2022-11-29 22:46:18 +0900

    feat : 전진을 기록한 리스트 구현.

--- src/main/kotlin/racingcar/calculate/RacingCalculater.kt
+++ src/main/kotlin/racingcar/calculate/RacingCalculater.kt
@@ -1,9 +1,16 @@
 package racingcar.calculate
 
+import racingcar.RacingParameter
+
 object RacingCalculater {
 
-    fun splitCarList(cars : String):List<String> = cars.split(",")
-
+    fun splitCarList(cars: String): List<String> = cars.split(",")
+    fun makeInitProgressList(cars: List<String>): MutableList<Int> {
+        val carProgress = mutableListOf<Int>()
+        for(count: Int in RacingParameter.START_COUNT.number .. cars.size)
+            carProgress.add(RacingParameter.START_COUNT.number)
+        return carProgress
+    }
 
 
 }
--- src/main/kotlin/racingcar/domain/RacingManager.kt
+++ src/main/kotlin/racingcar/domain/RacingManager.kt
@@ -5,17 +5,23 @@ import racingcar.IO.RacingOutput
 import racingcar.calculate.RacingCalculater
 
 object RacingManager {
-    lateinit var carList:List<String>
-    lateinit var carProgress:List<Int>
+    lateinit var carList: List<String>
+    lateinit var carProgress: MutableList<Int>
 
     fun gameStart() {
+        getCars()
 
     }
 
-    fun getCars() {
+    private fun getCars() {
         RacingOutput.giveMeCars()
         val cars = RacingInput.inputCar()
-        carList=RacingCalculater.splitCarList(cars)
+        carList = RacingCalculater.splitCarList(cars)
+        makeCarProgress()
+    }
+
+    private fun makeCarProgress() {
+        carProgress=RacingCalculater.makeInitProgressList(carList)
     }
 
 
