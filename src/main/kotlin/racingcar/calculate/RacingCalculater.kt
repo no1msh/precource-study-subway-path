158940a13593e20711532c8d9bde4775d55af5e5
meltnia <themtale@gmail.com>
2022-11-29 22:39:58 +0900

    feat : 받은 차를 리스트로 만드는 작업 추가.

--- src/main/kotlin/racingcar/calculate/RacingCalculater.kt
+++ src/main/kotlin/racingcar/calculate/RacingCalculater.kt
@@ -2,5 +2,8 @@ package racingcar.calculate
 
 object RacingCalculater {
 
+    fun splitCarList(cars : String):List<String> = cars.split(",")
+
+
 
 }
--- src/main/kotlin/racingcar/domain/RacingManager.kt
+++ src/main/kotlin/racingcar/domain/RacingManager.kt
@@ -2,8 +2,11 @@ package racingcar.domain
 
 import racingcar.IO.RacingInput
 import racingcar.IO.RacingOutput
+import racingcar.calculate.RacingCalculater
 
 object RacingManager {
+    lateinit var carList:List<String>
+    lateinit var carProgress:List<Int>
 
     fun gameStart() {
 
@@ -12,6 +15,7 @@ object RacingManager {
     fun getCars() {
         RacingOutput.giveMeCars()
         val cars = RacingInput.inputCar()
+        carList=RacingCalculater.splitCarList(cars)
     }
 
 
