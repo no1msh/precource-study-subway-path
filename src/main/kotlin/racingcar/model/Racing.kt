e016f3debef36057c1f309ce7dc1c0b15888c07a
no1msh <no1msh1217@gmail.com>
2022-12-01 20:52:46 +0900

    feat: 레이싱을 담당하는 클래스 생성 및 움직일 수 있는지 체크하는 메소드 구현

---
+++ src/main/kotlin/racingcar/model/Racing.kt
@@ -0,0 +1,20 @@
+package racingcar.model
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Racing(private val cars : List<Car>) {
+
+    private fun canMove() : Boolean{
+        val randomValue = Randoms.pickNumberInRange(START_RANGE , END_RANGE)
+        if (randomValue >= CUT_OFF){
+            return true
+        }
+        return false
+    }
+
+    companion object{
+        const val START_RANGE = 0
+        const val END_RANGE = 9
+        const val CUT_OFF = 4
+    }
+}
