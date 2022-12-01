165b3086b007e39a8b6c4bd50d7f3f2134f7873a
no1msh <no1msh1217@gmail.com>
2022-12-01 21:01:33 +0900

    feat: 레이스를 한번 진행하는 메소드 구현

--- src/main/kotlin/racingcar/model/Racing.kt
+++ src/main/kotlin/racingcar/model/Racing.kt
@@ -2,17 +2,25 @@ package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class Racing(private val cars : List<Car>) {
+class Racing(private val cars: List<Car>) {
 
-    private fun canMove() : Boolean{
-        val randomValue = Randoms.pickNumberInRange(START_RANGE , END_RANGE)
-        if (randomValue >= CUT_OFF){
+    private fun canMove(): Boolean {
+        val randomValue = Randoms.pickNumberInRange(START_RANGE, END_RANGE)
+        if (randomValue >= CUT_OFF) {
             return true
         }
         return false
     }
 
-    companion object{
+    fun raceOnce() {
+        for (car in this.cars) {
+            if (canMove()) {
+                car.move()
+            }
+        }
+    }
+
+    companion object {
         const val START_RANGE = 0
         const val END_RANGE = 9
         const val CUT_OFF = 4
