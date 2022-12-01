952eb063feb556c89a11a2b92ce073211779d048
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-12-01 16:41:56 +0900

    refactor(ExceptionHandler): 예외처리 상수 제거

--- src/main/kotlin/racingcar/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/racingcar/exceptions/ExceptionHandler.kt
@@ -1,15 +1,13 @@
 package racingcar.exceptions
 
 const val NAME_RANGE = 5
-const val NOT_INCLUSIVE_NAME_RANGE = "자동차 이름은 5글자 이하 여야 합니다."
-const val NOT_NUMBER = "숫자여야 합니다."
 
 object ExceptionHandler {
     fun checkCarName(carNames: List<String>) {
         carNames.forEach {
-            require(it.length <= NAME_RANGE) { NOT_INCLUSIVE_NAME_RANGE }
+            require(it.length <= NAME_RANGE) { "자동차 이름은 5글자 이하 여야 합니다." }
         }
     }
 
-    fun String.toIntOrThrow(): Int = requireNotNull(toIntOrNull()) { NOT_NUMBER }
+    fun String.toIntOrThrow(): Int = requireNotNull(toIntOrNull()) { "숫자여야 합니다." }
 }
