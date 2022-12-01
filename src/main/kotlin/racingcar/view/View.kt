1c6879ff5303be0af03aa15d7b07800aa88ac470
no1msh <no1msh1217@gmail.com>
2022-12-01 23:23:12 +0900

    feat: InputView와 OutputView를 통합하여 관리하는 객체 구현

---
+++ src/main/kotlin/racingcar/view/View.kt
@@ -0,0 +1,35 @@
+package racingcar.view
+
+import racingcar.model.Car
+
+object View {
+
+    private var totalResult = StringBuilder()
+
+    fun requestCarNames(): String {
+        UI.printRequestInputCarName()
+        return InputView.inputCars()
+    }
+
+    fun requestCount(): String {
+        UI.printRequestInputCount()
+        return InputView.inputCount()
+    }
+
+    fun printTotalResult() {
+        println()
+        UI.printResult()
+        println(totalResult)
+    }
+
+    fun addTempResult(cars: List<Car>) {
+        totalResult.append(OutputView.getTempResult(cars))
+    }
+
+    fun printWinner(winners: List<String>) {
+        val finalWinners = StringBuilder()
+        finalWinners.append("최종 우승자 : ")
+            .append(OutputView.getWinner(winners))
+        println(finalWinners.toString())
+    }
+}
