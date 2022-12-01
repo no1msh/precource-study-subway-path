f89002539a41cb8e0fd0cc11029aaf6dfc1b5467
no1msh <no1msh1217@gmail.com>
2022-12-01 23:19:44 +0900

    feat: Car 클래스 getter 구현

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -4,6 +4,9 @@ class Car(private val name: String) {
 
     private var position: Int = 0
 
+    fun getName() = this.name
+    fun getPosition() = this.position
+
     fun move() {
         position++
     }
