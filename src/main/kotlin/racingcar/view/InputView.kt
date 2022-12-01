4c70aa62f4b055ec7f387d62cb362906e142a17b
no1msh <no1msh1217@gmail.com>
2022-12-01 19:01:29 +0900

    feat: InputView 오브젝트 생성 및 자동차의 이름을 받는 메소드 구현

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,10 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+    fun inputCars(): List<String> {
+        val userInputCars = Console.readLine()
+        return userInputCars.split(",")
+    }
+}
