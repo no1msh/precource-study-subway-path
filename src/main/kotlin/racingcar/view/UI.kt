35cea53356a73deb9616c44881d23e08eb330904
no1msh <no1msh1217@gmail.com>
2022-12-01 18:34:20 +0900

    feat: UI에 해당하는 메소드들 구현

---
+++ src/main/kotlin/racingcar/view/UI.kt
@@ -0,0 +1,15 @@
+package racingcar.view
+
+object UI {
+    fun printRequestInputCarName() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    }
+
+    fun printRequestInputCount() {
+        println("시도할 회수는 몇회인가요?")
+    }
+
+    fun printResult() {
+        println("실행 결과")
+    }
+}
