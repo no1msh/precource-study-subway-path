69f5213403a002c0f37de9b58327ecb906ad6840
krrong <khk9664@naver.com>
2022-12-03 17:59:11 +0900

    feat(InputView): 이름의 길이가 5자 이하일 경우 예외 처리할 수 있도록 확인

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -7,6 +7,14 @@ class InputView {
     fun requireCarNames(): String {
         val carNames = Console.readLine()
         ExceptionHandler.checkNames(carNames)
+        val names = carNames.split(",")
+        isValidLength(names)
         return carNames
+    }
+
+    fun isValidLength(names: List<String>){
+        for(name in names){
+            ExceptionHandler.checkNameLength(name)
+        }
     }
 }
