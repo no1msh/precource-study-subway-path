2aba4f47ee47750bd290dc686af52079d4f40ea6
krrong <khk9664@naver.com>
2022-12-03 18:00:13 +0900

    test(ExceptionHandler): 자동차 이름 입력 정상/예외 테스트 코드 추가

--- src/test/kotlin/service/ExceptionHandlerTest.kt
+++ src/test/kotlin/service/ExceptionHandlerTest.kt
@@ -15,6 +15,22 @@ internal class ExceptionHandlerTest: NsTest() {
         }
     }
 
+    @Test
+    fun `자동차 이름 입력 시 알파벳과 쉼표만 입력했는지 확인2`() {
+        assertSimpleTest {
+            runException("pobi,woni,jin")
+            assertThat(output()).contains("")
+        }
+    }
+
+    @Test
+    fun `자동차 이름 입력 시 알파벳과 쉼표만 입력했는지 확인3`() {
+        assertSimpleTest {
+            runException("pobi,woniaasd,jin")
+            assertThat(output()).contains(Message.ERROR.toString())
+        }
+    }
+
     override fun runMain() {
         main()
     }
