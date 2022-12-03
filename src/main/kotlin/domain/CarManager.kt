80ff108adcb7f77fc5c0e209c5b697a6ef8ddfb8
krrong <khk9664@naver.com>
2022-12-03 19:05:10 +0900

    feat(CarManager): 이름을 입력 받고 자동차 생성하는 기능 구현

--- src/main/kotlin/domain/CarManager.kt
+++ src/main/kotlin/domain/CarManager.kt
@@ -1,12 +1,28 @@
 package domain
 
+import view.View
+
 class CarManager {
     private var cars: MutableList<Car> = mutableListOf()
+    private var tryCnt: Int = 0
+    private val view = View()
+
+    /**
+     * 사용자에게 이름을 입력받고 자동차 생성
+     */
+    fun makeCars() {
+        val carNames = view.requireCarNames()
+        for (name in carNames) {
+            makeCar(name)
+        }
+    }
 
     /**
      * 자동차 생성 및 CarManager 에 추가
      */
-    fun makeCar(name: String){
+    fun makeCar(name: String) {
         cars.add(Car(name))
     }
+
+
 }
