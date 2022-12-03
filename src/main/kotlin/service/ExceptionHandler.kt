8928fa98506b62f9475f4b6b023bb8bbb0d835fe
krrong <khk9664@naver.com>
2022-12-03 19:11:01 +0900

    refactor(ExceptionHandler): 재입력 할 수 있게 반환 값 추가

--- src/main/kotlin/service/ExceptionHandler.kt
+++ src/main/kotlin/service/ExceptionHandler.kt
@@ -4,18 +4,16 @@ object ExceptionHandler {
     /**
      * 자동차 이름 입력 시 알파벳과 쉼표만 입력했는지 확인
      */
-    fun checkNames(names: String) {
+    fun checkNames(names: String) : Boolean {
         try {
             for (i in names) {
-                require(
-                    i in 'A'..'Z'
-                            || i in 'a'..'z'
-                            || i == ','
-                ) { Message.ERROR.toString() }
+                require(i in 'A'..'Z'|| i in 'a'..'z' || i == ',') { Message.ERROR.toString() }
             }
         } catch (e: IllegalArgumentException) {
             println(e.message)
+            return false
         }
+        return true
     }
 
     /**
