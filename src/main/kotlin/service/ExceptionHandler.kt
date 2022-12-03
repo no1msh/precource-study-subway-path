b9a217587cf765b74f51fc046f44b04128eb7842
krrong <khk9664@naver.com>
2022-12-03 17:58:54 +0900

    feat(ExceptionHandler): 이름의 길이가 5자 이하일 경우 예외 처리

--- src/main/kotlin/service/ExceptionHandler.kt
+++ src/main/kotlin/service/ExceptionHandler.kt
@@ -15,4 +15,17 @@ object ExceptionHandler {
             println(e.message)
         }
     }
+
+    /**
+     * 이름의 길이가 5자 이하인지 확인
+     */
+    fun checkNameLength(name: String) {
+        try{
+            require(name.length <= 5){
+                Message.ERROR.toString()
+            }
+        }catch (e:IllegalArgumentException){
+            println(e.message)
+        }
+    }
 }
