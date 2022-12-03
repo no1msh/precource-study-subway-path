17a5b832cf9c7727daff39aa6504bd0df8226cae
krrong <khk9664@naver.com>
2022-12-03 19:38:18 +0900

    feat(Car): 무작위 값이 4 이상일 경우 이동하는 기능 구현

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,7 +1,22 @@
 package domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(private val name: String) {
 
     private var position: Int = 0
 
+    /**
+     * 조건 이상일 경우 이동하는 기능
+     */
+    fun move() {
+        val canMove = Randoms.pickNumberInRange(0, 9)
+        if (canMove >= Condition.STANDARD.value) {
+            position++
+        }
+    }
+
+    enum class Condition(val value: Int) {
+        STANDARD(4)
+    }
 }
