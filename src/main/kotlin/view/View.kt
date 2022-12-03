42782d55de2c3fed9b0df8c5f8a62eefe3ff0299
krrong <khk9664@naver.com>
2022-12-03 19:58:50 +0900

    feat(View): 자동차들의 차수별 실행 결과 출력 기능 구현

--- src/main/kotlin/view/View.kt
+++ src/main/kotlin/view/View.kt
@@ -1,5 +1,7 @@
 package view
 
+import domain.Car
+
 class View {
     private val outputView = OutputView()
     private val inputView = InputView()
@@ -12,5 +14,12 @@ class View {
     fun requireTryCnt(): Int {
         outputView.printRequireTry()
         return inputView.requireTryCnt()
+    }
+
+    fun printResult(cars: List<Car>) {
+        for(car in cars){
+            outputView.printResult(car)
+        }
+        println()
     }
 }
