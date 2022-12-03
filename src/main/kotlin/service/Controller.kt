5ad9779cdff7f14e495922e1bd0b1cdf0f506f44
krrong <khk9664@naver.com>
2022-12-03 20:12:38 +0900

    feat(Controller): 게임 로직 구성

--- src/main/kotlin/service/Controller.kt
+++ src/main/kotlin/service/Controller.kt
@@ -1,7 +1,7 @@
 package service
 
 import domain.CarManager
-import view.View
+import view.OutputView
 
 class Controller {
     private val carManager = CarManager()
@@ -9,5 +9,10 @@ class Controller {
     fun play() {
         carManager.getNamesAndMakeCars()
         carManager.getTryCnt()
+        do {
+            carManager.nextStep()
+        } while (carManager.confirmWinners().isEmpty())
+        val winner = carManager.confirmWinners()
+        OutputView().printResult(winner)
     }
 }
