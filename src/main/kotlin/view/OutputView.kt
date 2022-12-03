083bbd7d1ed7e2d54b902e4753ceea98f1b4274f
krrong <khk9664@naver.com>
2022-12-03 19:58:37 +0900

    feat(outputView): 자동차 하나의 차수별 실행 결과 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,13 +1,19 @@
 package view
 
+import domain.Car
 import service.Message
 
 class OutputView {
-    fun printRequireCarNames(){
+    fun printRequireCarNames() {
         println(Message.REQUIRE_CAR_NAME.toString())
     }
 
-    fun printRequireTry(){
+    fun printRequireTry() {
         println(Message.REQUIRE_TRY.toString())
+    }
+
+    fun printResult(car: Car) {
+        print(car.getName() + " : ")
+        println("-".repeat(car.getCurrentPosition()))
     }
 }
