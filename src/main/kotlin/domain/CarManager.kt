471c0233e4aff7ac56cadbc5eca26194d38ae9d4
krrong <khk9664@naver.com>
2022-12-04 15:55:23 +0900

    refactor(CarManager): 클래스에서 View를 사용하지 않도록 수정

--- src/main/kotlin/domain/CarManager.kt
+++ src/main/kotlin/domain/CarManager.kt
@@ -1,27 +1,23 @@
 package domain
 
-import view.View
-
 class CarManager {
     private var cars: MutableList<Car> = mutableListOf()
     private var tryCnt: Int = 0
-    private val view = View()
 
     /**
-     * 사용자에게 이름을 입력받고 자동차 생성
+     * 자동차 생성
      */
-    fun getNamesAndMakeCars() {
-        val carNames = view.requireCarNames()
+    fun getNamesAndMakeCars(carNames: List<String>) {
         for (name in carNames) {
             makeCar(name)
         }
     }
 
     /**
-     * 시도 횟수 입력
+     * 시도 횟수
      */
-    fun getTryCnt() {
-        tryCnt = view.requireTryCnt()
+    fun getTryCnt(cnt: Int) {
+        tryCnt = cnt
     }
 
     /**
@@ -44,6 +40,10 @@ class CarManager {
         return firstPlayers
     }
 
+    fun getCars(): MutableList<Car> {
+        return this.cars
+    }
+
     /**
      * 자동차별로 무작위 숫자를 뽑고 조건 이상인 경우 전진
      */
@@ -51,6 +51,5 @@ class CarManager {
         for (car in cars) {
             car.move()
         }
-        view.printStep(cars)
     }
 }
